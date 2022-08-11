b1e52823446dd47618b71420ec9a322fea25bf7a
Vitaliy Zarubin <zarubin@surf.dev>
2022-08-11 18:22:06 +0300

    Update gradle dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,58 +1,16 @@
 plugins {
     application
-    kotlin("jvm") version "1.7.10"
-    id("org.jetbrains.kotlin.plugin.serialization") version "1.7.10"
+    kotlin("jvm")
+    id("org.jetbrains.kotlin.plugin.serialization")
+    id("org.jetbrains.dokka")
 }
 
 group = "com.keygenqt"
 version = "0.0.1"
 
-repositories {
-    mavenCentral()
-    maven { url = uri("https://maven.pkg.jetbrains.space/public/p/ktor/eap") }
-}
-
 dependencies {
-
-    //////////////////////////////////
-    //// Ktor
-    val ktorVersion: String by project
-    val kotlinVersion: String by project
-
-    implementation("io.ktor:ktor-server-content-negotiation-jvm:$ktorVersion")
-    implementation("io.ktor:ktor-server-core-jvm:$ktorVersion")
-    implementation("io.ktor:ktor-serialization-kotlinx-json-jvm:$ktorVersion")
-    implementation("io.ktor:ktor-server-auth-jvm:$ktorVersion")
-    implementation("io.ktor:ktor-server-netty-jvm:$ktorVersion")
-    testImplementation("io.ktor:ktor-server-tests-jvm:$ktorVersion")
-    testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlinVersion")
-
-    //////////////////////////////////
-    //// DI
-    val koinVersion: String by project
-
-    implementation("io.insert-koin:koin-ktor:$koinVersion")
-
-    //////////////////////////////////
-    //// DB
-    val mysqlConnectorVersion: String by project
-    val hikariCpVersion: String by project
-    val flywayVersion: String by project
-    val exposedVersion: String by project
-
-    implementation("mysql:mysql-connector-java:$mysqlConnectorVersion")
-    implementation("com.zaxxer:HikariCP:$hikariCpVersion")
-
-    implementation("org.jetbrains.exposed:exposed-core:$exposedVersion")
-    implementation("org.jetbrains.exposed:exposed-dao:$exposedVersion")
-    implementation("org.jetbrains.exposed:exposed-jdbc:$exposedVersion")
-
-    implementation("org.flywaydb:flyway-core:$flywayVersion")
-    implementation("org.flywaydb:flyway-mysql:$flywayVersion")
-
-    //////////////////////////////////
-    //// Other
-    val logbackVersion: String by project
-
-    implementation("ch.qos.logback:logback-classic:$logbackVersion")
+    implementation(libs.bundles.ktor)
+    implementation(libs.bundles.other)
+    implementation(libs.bundles.database)
+    testImplementation(libs.bundles.tests)
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,31 @@
+pluginManagement {
+
+    val kotlinVersion: String by settings
+    val dokkaVersions: String by settings
+    val spotlessVersion: String by settings
+
+    plugins {
+        kotlin("jvm") version kotlinVersion
+        id("org.jetbrains.dokka") version dokkaVersions
+        id("com.diffplug.spotless") version spotlessVersion
+        id("org.jetbrains.kotlin.plugin.serialization") version kotlinVersion
+    }
+}
+
+dependencyResolutionManagement {
+
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+
+    repositories {
+        mavenCentral()
+        maven { url = uri("https://maven.pkg.jetbrains.space/public/p/ktor/eap") }
+    }
+
+    versionCatalogs {
+        create("libs") {
+            from(fileTree("dependencies"))
+        }
+    }
+}
+
 rootProject.name = "vz-api"
